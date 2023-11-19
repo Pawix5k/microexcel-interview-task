d0779a8b8388ce1df088c28edb08ecac9fded6e8
guarando <pawelpurzyk@gmail.com>
2023-11-19 10:59:54 +0100

    Add more Sheet tests

--- test/test_sheet.py
+++ test/test_sheet.py
@@ -43,3 +43,45 @@ class TestTokenizer(unittest.TestCase):
                 ["1", "FALSE", "TRUE"],
             ]
             self.assertEqual(sheet.evaluate(), expected)
+
+        with self.subTest(i=3):
+            input_ = [
+                ["=B1", "=A2"],
+                ["=B2", "=IF()"],
+            ]
+            sheet = Sheet(input_)
+            expected = [
+                ["ERROR", "ERROR"],
+                ["ERROR", "ERROR"],
+            ]
+            self.assertEqual(sheet.evaluate(), expected)
+
+        with self.subTest(i=4):
+            input_ = [
+                ["=B1", "=A2"],
+                ["=B2", "=NONEXISTENT()"],
+            ]
+            sheet = Sheet(input_)
+            expected = [
+                ["ERROR", "ERROR"],
+                ["ERROR", "ERROR"],
+            ]
+            self.assertEqual(sheet.evaluate(), expected)
+
+        with self.subTest(i=5):
+            input_ = [["=+A2"]]
+            sheet = Sheet(input_)
+            expected = [["ERROR"]]
+            self.assertEqual(sheet.evaluate(), expected)
+
+        with self.subTest(i=6):
+            input_ = [['=CONCATENATE("Bar"; 4; TRUE; "Foo")']]
+            sheet = Sheet(input_)
+            expected = [["Bar4TRUEFoo"]]
+            self.assertEqual(sheet.evaluate(), expected)
+
+        with self.subTest(i=7):
+            input_ = [["=DFB345 + 5"]]
+            sheet = Sheet(input_)
+            expected = [["5"]]
+            self.assertEqual(sheet.evaluate(), expected)
